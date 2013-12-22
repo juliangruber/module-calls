6779cd52a850be298d8e481a2a3b88c0bb55b802
Julian Gruber <julian@juliangruber.com>
2013-12-22 16:46:20 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,128 @@
+
+/**
+ * Module dependencies.
+ */
+
+var astw = require('astw');
+var codegen = require('escodegen').generate;
+var debug = require('debug')('module-calls');
+
+/**
+ * Expose `find`.
+ */
+
+module.exports = find;
+
+/**
+ * Find all places module `name` is used in `code`.
+ *
+ * @param {String} name
+ * @param {String} code
+ * @return {Array}
+ */
+
+function find(name, code) {
+  var calls = [];
+  var names = {};
+  var walk = astw(code);
+  
+  walk(function(node) {
+    if (isRequire(node) && name == node.arguments[0].value) {
+      if ('VariableDeclarator' == node.parent.type) {
+        // var name = require('name');
+        names[node.parent.id.name] = true;
+        var code = 'var ' + codegen(node.parent) + ';';
+        debug('declaration require: %s', code);
+        calls.push({ code: code });
+      } else if ('AssignmentExpression' == node.parent.type) {
+        // name = require('name');
+        names[node.parent.left.name] = true;
+        var code = codegen(node.parent) + ';';
+        debug('assignemt require: %s', code);
+        calls.push({ code: code });
+      } else if ('CallExpression' == node.parent.type) {
+        // require('name')(arg);
+        node.parent.arguments = shortenCallbacks(node.parent.arguments);
+        var code = codegen(node.parent.parent);
+        debug('require call: %s', code);
+        calls.push({ code: code });
+      } else {
+        // require('name');
+        var code = codegen(node.parent);
+        debug('require: %s', code);
+        calls.push({ code: code });
+      }
+    } else if ('CallExpression' == node.type && names[node.callee.name]) {
+      node.arguments = shortenCallbacks(node.arguments);
+      
+      if ('VariableDeclarator' == node.parent.type) {
+        // var ret = name(arg);
+        var code = 'var ' + codegen(node.parent) + ';';
+        debug('declaration: %s', code);
+        calls.push({ code: code });
+      } else if ('AssignmentExpression' == node.parent.type) {
+        // ret = name(arg);
+        var code = codegen(node.parent) + ';';
+        debug('assignment: %s', code);
+        calls.push({ code: code });
+      } else {
+        // name(arg)
+        var code = codegen(node.parent);
+        debug('call: %s', code);
+        calls.push({ code: code });
+      }
+    }
+  });
+  
+  return calls;
+}
+
+/**
+ * Shorten callbacks from
+ *
+ *   function(arg) {
+ *       some();
+ *       code();
+ *   }
+ *
+ * to
+ *
+ *   function(arg) {
+ *       "callback...";
+ *   }
+ *
+ * @param {Array} args
+ * @return {Array}
+ * @api private
+ */
+
+function shortenCallbacks(args) {
+  return args.map(function(arg) {
+    if ('FunctionExpression' == arg.type) {
+      arg.body = {
+        type: 'BlockStatement',
+        body: [{
+          type: 'ExpressionStatement',
+          expression: {
+            type: 'Literal', value: 'callback...'
+          }
+        }]
+      };
+      return arg;
+    } else {
+      return arg;
+    }
+  });
+}
+
+/**
+ * Check if `node` is a `require` call.
+ *
+ * @param {Node} node
+ * @return {Boolean}
+ * @api private
+ */
+
+function isRequire(node) {
+  return 'CallExpression' == node.type && 'require' == node.callee.name;
+}
---
+++ test.js
@@ -0,0 +1,141 @@
+var test = require('tape');
+var findCalls = require('./');
+
+test('no calls', function(t) {
+  var calls = findCalls('name', '"nope"');
+  t.deepEqual(calls, []);
+  t.end();
+});
+
+test('require', function(t) {
+  var source = 'require(\'name\');';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'require(\'name\');' }
+  ]);
+  t.end();
+});
+
+test('declaration require', function(t) {
+  var source = 'var name = require(\'name\');';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var name = require(\'name\');' }
+  ]);
+  t.end();
+});
+
+test('multi declaration require', function(t) {
+  var source = 'var x = y,\nname = require(\'name\');';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var name = require(\'name\');' }
+  ]);
+  t.end();
+});
+
+test('assign require', function(t) {
+  var source = 'name = require(\'name\');';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'name = require(\'name\');' }
+  ]);
+  t.end();
+});
+
+test('multi assignment require', function(t) {
+  var source = 'x = y,\nname = require(\'name\');';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'name = require(\'name\');' }
+  ]);
+  t.end();
+});
+
+test('function call', function(t) {
+  var source = 'var name = require(\'name\'); name(arg);';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var name = require(\'name\');' },
+    { code: 'name(arg);' }
+  ]);
+  t.end();
+});
+
+test('require call', function(t) {
+  var source = 'require(\'name\')(arg);';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'require(\'name\')(arg);' }
+  ]);
+  t.end();
+});
+
+test('declaration', function(t) {
+  var source = 'var name = require(\'name\'); var ret = name(arg);';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var name = require(\'name\');' },
+    { code: 'var ret = name(arg);' }
+  ]);
+  t.end();
+});
+
+test('multi declaration', function(t) {
+  var source = 'var name = require(\'name\'); var foo = bar, ret = name(arg);';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var name = require(\'name\');' },
+    { code: 'var ret = name(arg);' }
+  ]);
+  t.end();
+});
+
+test('assignment', function(t) {
+  var source = 'var name = require(\'name\'); ret = name(arg);';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var name = require(\'name\');' },
+    { code: 'ret = name(arg);' }
+  ]);
+  t.end();
+});
+
+test('multi assignment', function(t) {
+  var source = 'var name = require(\'name\'); foo = bar, ret = name(arg);';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var name = require(\'name\');' },
+    { code: 'ret = name(arg);' }
+  ]);
+  t.end();
+});
+
+test('aliased', function(t) {
+  var source = 'var alias = require(\'name\'); alias(arg);';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var alias = require(\'name\');' },
+    { code: 'alias(arg);' }
+  ]);
+  t.end();
+});
+
+test('callback in function call', function(t) {
+  var source = 'var name = require(\'name\'); name(function(arg){\n"bar"\n});';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'var name = require(\'name\');' },
+    { code: 'name(function (arg) {\n    \'callback...\';\n});' }
+  ]);
+  t.end();
+});
+
+test('callback in require call', function(t) {
+  var source = 'require(\'name\')(function(arg){\n"bar"\n});';
+  var calls = findCalls('name', source);
+  t.deepEqual(calls, [
+    { code: 'require(\'name\')(function (arg) {\n    \'callback...\';\n});' }
+  ]);
+  t.end();
+});
